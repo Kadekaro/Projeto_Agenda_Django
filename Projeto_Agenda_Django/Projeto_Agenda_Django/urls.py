c5f227bd0c2d080df9d6c4a36107ab4d7b8efeb0
kadekaro <kadekaroshop@gmail.com>
2023-09-13 13:16:08 -0300

    fazendo o login e logout e customizando a página

--- Projeto_Agenda_Django/Projeto_Agenda_Django/urls.py
+++ Projeto_Agenda_Django/Projeto_Agenda_Django/urls.py
@@ -23,5 +23,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("agenda/", views.lista_eventos),
     # path("", views.index_redirect), ABAIXO COLOCO UMA FORMA DE REDIRECIONAMENTO DIRETO NO URL
-    path("", RedirectView.as_view(url='/agenda/'))
+    path("", RedirectView.as_view(url='/agenda/')),
+    path("login/", views.login_user),
+    path("login/submit", views.submit_login),
+    path("logout/", views.logout_user),
 ]
