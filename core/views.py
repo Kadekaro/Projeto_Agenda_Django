d8f81b8af36f1e6028e030bd0f487a7a1850bb08
kadekaro <kadekaroshop@gmail.com>
2023-09-12 23:49:24 -0300

    ATUALIZANDO COM PAGINAÇÃO

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
+
 
 # Create your views here.
+
+# def index_redirect():       AQUI FOI SO UMA MANEIRA DE REDIRECIONAR A PÁGINA QUANDO A URL TA PURA
+#    return redirect('/agenda/')
+
+
+def lista_eventos(request):
+    # evento = Evento.objects.get(id=1)
+    # evento = Evento.objects.all()
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos': evento}
+    return render(request, 'agenda/agenda.html', dados)
