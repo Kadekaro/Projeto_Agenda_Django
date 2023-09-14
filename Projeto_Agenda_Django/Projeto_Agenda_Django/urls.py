79e77684961a1cc72b4f7aba50566e751145be2c
kadekaro <kadekaroshop@gmail.com>
2023-09-13 23:37:50 -0300

    Adicionando funções de alteração e exclusão de dados

--- Projeto_Agenda_Django/Projeto_Agenda_Django/urls.py
+++ Projeto_Agenda_Django/Projeto_Agenda_Django/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("agenda/", views.lista_eventos),
     path("agenda/evento/", views.eventos),
     path("agenda/evento/submit", views.submit_eventos),
+    path("agenda/evento/delete/<int:id_evento>/", views.delete_evento),
     # path("", views.index_redirect), ABAIXO COLOCO UMA FORMA DE REDIRECIONAMENTO DIRETO NO URL
     path("", RedirectView.as_view(url='/agenda/')),
     path("login/", views.login_user),
